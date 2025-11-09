a805836b23f6f2060c5c5084a6968b82279faf90
Dylan Yagninim <dylanmagnym@gmail.com>
2025-11-09 15:22:06 -0500

    Implement the Fish Prefab and Placement Logic

---
+++ Assets/Scripts/CollectionSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+
+public class CollectionSystem : MonoBehaviour
+{
+    public GameObject prefabFish;
+    private List<GameObject> fishOnScreen = new List<GameObject>();
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for right mouse button click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            // Adjust Z for ScreenToWorldPoint conversion
+            mousePosition.z = -Camera.main.transform.position.z;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            spawnFish(worldPosition);
+        }
+    }
+
+    void spawnFish(Vector3 worldPosition)
+    {
+        if (prefabFish != null)
+        {
+            // Instantiate the new fish
+            GameObject fish = Instantiate<GameObject>(prefabFish);
+            fish.transform.position = worldPosition;
+            // Add it to the list (which tracks the spawn order)
+            fishOnScreen.Add(fish);
+        }
+    }
+}
--- Assets/Scripts/shipcod.cs
+++ Assets/Scripts/shipcod.cs
@@ -10,18 +10,24 @@ public class shipcod : MonoBehaviour
     private float shipRadius;
     private const float RotateDegreePerSecond = 48f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        // Assuming ScreenUtils and CircleCollider2D are available/configured
         shipRadius = GetComponent<CircleCollider2D>().radius;
     }
 
     private void FixedUpdate()
     {
-        //thrust
-        if (Input.GetKey(KeyCode.Space) == true)
+        //thrust
+        if (Input.GetKey(KeyCode.Space) == true)
         {
+            // Debug Log 1: Thrust application
+            Debug.Log("Thrust is applied when the space bar is pressed or held");
+            // Debug Log 2: Thrust direction
+            Debug.Log("Thrust is applied in the direction the ship is facing. Thrust Direction: " + ThrustDirection);
+
             rb2d.AddForce(ThrustDirection.normalized * ThrustForce);
         }
     }
@@ -29,42 +35,63 @@ public class shipcod : MonoBehaviour
     void OnBecameInvisible()
     {
         Vector2 newPosition = transform.position;
+        bool wrapped = false; // Flag to track if wrapping occurred
 
-        // Exited LEFT
-        if (newPosition.x < ScreenUtils.ScreenLeft - shipRadius)
+        // Exited LEFT
+        if (newPosition.x < ScreenUtils.ScreenLeft - shipRadius)
         {
             newPosition.x = ScreenUtils.ScreenRight + shipRadius;
+            wrapped = true;
         }
-        // Exited RIGHT
-        else if (newPosition.x > ScreenUtils.ScreenRight + shipRadius)
+        // Exited RIGHT
+        else if (newPosition.x > ScreenUtils.ScreenRight + shipRadius)
         {
             newPosition.x = ScreenUtils.ScreenLeft - shipRadius;
+            wrapped = true;
         }
 
-        // Exited BOTTOM
-        if (newPosition.y < ScreenUtils.ScreenBottom - shipRadius)
+        // Exited BOTTOM
+        if (newPosition.y < ScreenUtils.ScreenBottom - shipRadius)
         {
             newPosition.y = ScreenUtils.ScreenTop + shipRadius;
+            wrapped = true;
         }
-        // Exited TOP
-        else if (newPosition.y > ScreenUtils.ScreenTop + shipRadius)
+        // Exited TOP
+        else if (newPosition.y > ScreenUtils.ScreenTop + shipRadius)
         {
             newPosition.y = ScreenUtils.ScreenBottom - shipRadius;
+            wrapped = true;
         }
 
         transform.position = newPosition;
+
+        // Debug Log 4: Screen wrapping
+        if (wrapped)
+        {
+            Debug.Log("The ship screen wraps properly. Wrapping should work correctly for the left, right, top, and bottom sides and all four corners. New position: " + newPosition);
+        }
     }
 
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame
+    void Update()
     {
-
+        // The "beyblade" axis likely maps to Left/Right arrow keys or 'A'/'D'
         float rotationInput = Input.GetAxis("beyblade");
 
-        // Check if the player is pressing a rotation button
-        if (Mathf.Abs(rotationInput) > 0)
+        // Check if the player is pressing a rotation button
+        if (Mathf.Abs(rotationInput) > 0)
         {
+            // Debug Log 3: Ship rotation
+            if (rotationInput < 0)
+            {
+                Debug.Log("The ship rotates counter-clockwise when the left key is pressed or held");
+            }
+            else // rotationInput > 0
+            {
+                Debug.Log("The ship rotates clockwise when the right key is pressed or held");
+            }
+
             float rotationAmount = RotateDegreePerSecond * Time.deltaTime;
             if (rotationInput < 0)
             {
